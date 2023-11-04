bc642c52291910b6edefe0926a4365557c70647d
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2023-11-05 01:49:48 +0300

    lab5 final

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -1,6 +1,4 @@
-﻿// lab5.cpp : Определяет точку входа для приложения.
-//
-
+﻿
 #include "framework.h"
 #include "lab5.h"
 #include <stdio.h>
@@ -12,6 +10,7 @@
 #define IDC_BUTTON_CREATE_KEY 1102
 #define IDC_BUTTON_DELETE_KEY 1103
 #define IDC_BUTTON_ADD_NEW_PARAMETER 1104
+#define IDC_BUTTON_SEARCH_VALUE 1105
 
 #define MAX_LOADSTRING 100
 
@@ -19,8 +18,7 @@ HWND hWnd;
 HMENU hMenu;
 HWND hEdit;
 HWND hButton, hButtonDelete, hButtonAddNewParameter;
-//HWND hValueNameEdit, hStringValueRadio, hDwordValueRadio;
-//HWND hMainWindow;
+HWND hEditValue;
 
 HWND hComboBox;
 HWND hValueDataEdit;
@@ -42,11 +40,12 @@ void LoadRegistryInfoToListBox(HWND hListBox, LPCWSTR subKey);
 bool DeleteRegistryKey(HKEY hKey, LPCWSTR subKey);
 bool SetRegistryValue(HKEY hKey, LPCSTR subKey, LPCSTR valueName, DWORD valueType, const BYTE* valueData, DWORD dataSize);
 bool CreateRegistryValue();
+void getRegistryValue(LPCTSTR subKey, LPCTSTR name);
 
-// Глобальные переменные:
-HINSTANCE hInst;                                // текущий экземпляр
-WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+HINSTANCE hInst;                                
+WCHAR szTitle[MAX_LOADSTRING];                  
+WCHAR szWindowClass[MAX_LOADSTRING];            
 
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -112,7 +111,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    const wchar_t CLASS_NAME[] = L"RegistryEditor";
 
-   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+   hInst = hInstance; 
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
@@ -122,12 +121,24 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
       return FALSE;
    }
 
+   HWND hStatic = CreateWindowEx(
+       0,
+       L"STATIC",
+       L"ключ:",
+       WS_CHILD | WS_VISIBLE,
+       10, 10, 50, 25, 
+       hWnd,
+       NULL,
+       hInstance,
+       NULL
+   );
+
    hEdit = CreateWindowEx(
        WS_EX_CLIENTEDGE,
        L"EDIT",
        L"",
        WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
-       10, 10, 300, 25,
+       70, 10, 300, 25,
        hWnd,
        (HMENU)IDC_EDIT_SUBKEY,
        hInstance,
@@ -151,9 +162,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
        L"BUTTON",
        L"Удалить ключ",
        WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
-       140, 45, 120, 30, // Новые координаты (140, 45)
+       140, 45, 120, 30, 
        hWnd,
-       (HMENU)IDC_BUTTON_DELETE_KEY, // Новый идентификатор
+       (HMENU)IDC_BUTTON_DELETE_KEY,
        hInstance,
        NULL
    );
@@ -163,9 +174,66 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
        L"BUTTON",
        L"Создать параметр",
        WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
-       270, 45, 120, 30, // Новые координаты (270, 45)
+       270, 45, 140, 30,
        hWnd,
-       (HMENU)IDC_BUTTON_ADD_NEW_PARAMETER, // Новый идентификатор
+       (HMENU)IDC_BUTTON_ADD_NEW_PARAMETER, 
+       hInstance,
+       NULL
+   );
+
+   HWND hStaticParamType = CreateWindow(
+       L"STATIC",
+       L"Тип параметра:",
+       WS_CHILD | WS_VISIBLE,
+       10, 100, 160, 25,
+       hWnd,
+       NULL,
+       hInstance,
+       NULL
+   );
+
+   HWND hStaticValueData = CreateWindow(
+       L"STATIC",
+       L"Значение параметра:",
+       WS_CHILD | WS_VISIBLE,
+       10, 130, 160, 25, 
+       hWnd,
+       NULL,
+       hInstance,
+       NULL
+   );
+
+   HWND hStaticValueName = CreateWindow(
+       L"STATIC",
+       L"Имя параметра:",
+       WS_CHILD | WS_VISIBLE,
+       10, 160, 160, 25,
+       hWnd,
+       NULL,
+       hInstance,
+       NULL
+   );
+
+   hEditValue = CreateWindowEx(
+       WS_EX_CLIENTEDGE,
+       L"EDIT",
+       L"",
+       WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
+       10, 200, 150, 25,
+       hWnd,
+       (HMENU)IDC_EDIT_SUBKEY,
+       hInstance,
+       NULL
+   );
+
+   HWND hButtonSearch = CreateWindowEx(
+       0,
+       L"BUTTON",
+       L"Найти значение",
+       WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+       220, 200, 120, 30,
+       hWnd,
+       (HMENU)IDC_BUTTON_SEARCH_VALUE,
        hInstance,
        NULL
    );
@@ -186,38 +254,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
     {
-        //hValueNameEdit = CreateWindowEx(0, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
-        //    50, 50, 200, 25, hWnd, NULL, NULL, NULL);
-
-        //// Радиокнопка для строки (REG_SZ)
-        //hStringValueRadio = CreateWindowEx(0, L"BUTTON", L"String Value", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-        //    50, 100, 150, 25, hWnd, NULL, NULL, NULL);
-        //SendMessage(hStringValueRadio, BM_SETCHECK, BST_CHECKED, 0);
-
-        //// Радиокнопка для DWORD (REG_DWORD)
-        //hDwordValueRadio = CreateWindowEx(0, L"BUTTON", L"DWORD Value", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
-        //    50, 125, 150, 25, hWnd, NULL, NULL, NULL);
-
-        //// Кнопка "Изменить значение"
-        //CreateWindowEx(0, L"BUTTON", L"Change Value", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        //    50, 175, 100, 25, hWnd, (HMENU)1, NULL, NULL);
-
-        //            // Поле для ввода subKey
-        //hSubKeyEdit = CreateWindowEx(0, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
-        //    50, 50, 200, 25, hWnd, NULL, NULL, NULL);
-
-        //// Кнопка "Получить значения"
-        //CreateWindowEx(0, L"BUTTON", L"Get Values", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        //    50, 100, 100, 25, hWnd, (HMENU)1, NULL, NULL);
-
-        //// `listbox` для отображения значений
-        //hValueList = CreateWindowEx(0, L"LISTBOX", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | LBS_DISABLENOSCROLL | LBS_HASSTRINGS | LBS_NOINTEGRALHEIGHT,
-        //    50, 150, 300, 200, hWnd, NULL, NULL, NULL);
-
         hComboBox = CreateWindow(TEXT("ComboBox"), NULL, WS_VISIBLE | WS_CHILD | CBS_DROPDOWNLIST,
-            10, 100, 200, 200, hWnd, NULL, NULL, NULL);
+            175, 100, 200, 200, hWnd, NULL, NULL, NULL);
 
-        // Добавление элементов в выпадающий список с соответствующими типами данных
         SendMessage(hComboBox, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(TEXT("Строковый параметр")));
         SendMessage(hComboBox, CB_SETITEMDATA, 0, REG_SZ);
         SendMessage(hComboBox, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(TEXT("Параметр DWORD(32 бита)")));
@@ -225,13 +264,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         SendMessage(hComboBox, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(TEXT("Параметр QWORD(64 бита)")));
         SendMessage(hComboBox, CB_SETITEMDATA, 2, REG_QWORD);
 
-        // Создание поля ввода для значения данных
         hValueDataEdit = CreateWindow(TEXT("EDIT"), NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-            10, 200, 150, 20, hWnd, NULL, NULL, NULL);
+            175, 130, 150, 20, hWnd, NULL, NULL, NULL);
 
-        // Создание поля ввода для имени значения
         hValueNameEdit = CreateWindow(TEXT("EDIT"), NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-            10, 300, 150, 20, hWnd, NULL, NULL, NULL);
+            175, 160, 150, 20, hWnd, NULL, NULL, NULL);
 
     }
     break;
@@ -253,10 +290,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             {
                 MessageBox(hWnd, L"Ошибка при создании ключа в реестре.", L"Ошибка", MB_OK | MB_ICONERROR);
             }
-            delete[] subKey; // Не забудьте освободить память
+            delete[] subKey;
         }
         break;
 
+        case IDC_BUTTON_SEARCH_VALUE:
+        {
+
+            int length = GetWindowTextLength(hEditValue);
+            TCHAR* buffer = new TCHAR[length + 1];
+
+            GetWindowText(hEditValue, buffer, length + 1);
+
+            int length2 = GetWindowTextLength(hEdit);
+            TCHAR* buffer2 = new TCHAR[length2 + 1];
+
+            GetWindowText(hEdit, buffer2, length2 + 1);
+
+            getRegistryValue(buffer2, buffer);
+
+        }
+        break;
         case IDC_BUTTON_DELETE_KEY:
         {
             int length = GetWindowTextLength(hEdit);
@@ -265,7 +319,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             
             HKEY hKey;
             if (RegOpenKeyExW(HKEY_CURRENT_USER, NULL, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS) {
-                // Удалить подключ реестра
                 if (DeleteRegistryKey(hKey, subKey)) {
                     MessageBox(hWnd, L"Ключ реестра успешно удален.", L"Успех", MB_OK | MB_ICONINFORMATION);
                 }
@@ -273,41 +326,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     MessageBox(hWnd, L"Не удалось удалить ключ реестра.", L"Ошибка", MB_OK | MB_ICONINFORMATION);
                 }
 
-                // Закрыть ключ реестра
                 RegCloseKey(hKey);
             }
             else {
                 MessageBox(hWnd, L"Не удалось открыть ключ реестра.", L"Ошибка", MB_OK | MB_ICONINFORMATION);
             }
 
-            delete[] subKey; // Не забудьте освободить память
+            delete[] subKey;
         }
         break;
         case IDC_BUTTON_ADD_NEW_PARAMETER:
         {
-            //int length = GetWindowTextLength(hEdit);
-            //LPWSTR subKey = new WCHAR[length + 1];
-            //GetWindowText(hEdit, subKey, length + 1);
-
-
-
-            //LPCSTR subKey = "Software\\MyApp";
-
-            //LPCSTR valueName = "MyValue";
-            //DWORD valueType = REG_SZ;
-            //const BYTE* valueData = (const BYTE*)"Hello, World!";
-            //DWORD dataSize = strlen((const char*)valueData) + 1;
-
-            //if (SetRegistryValue(HKEY_CURRENT_USER, subKey, valueName, valueType, valueData, dataSize)) {
-            //    MessageBox(hWnd, L"Successfully created registry key and set value.", L"Успех", MB_OK | MB_ICONINFORMATION);
-            //}
-
             CreateRegistryValue();
         }
         break;
         }
         break;
-        // Остальные случаи обработки команд
     }
 
     case WM_PAINT:
@@ -345,7 +379,6 @@ bool CreateRegistryKey(HKEY hRootKey, LPCWSTR subKey)
 }
 
 bool DeleteRegistryKey(HKEY hKey, LPCWSTR subKey) {
-    // Удалить ключ реестра
     if (RegDeleteKeyW(hKey, subKey) == ERROR_SUCCESS) {
         return true;
     }
@@ -378,22 +411,24 @@ bool SetRegistryValue(HKEY hKey, LPCSTR subKey, LPCSTR valueName, DWORD valueTyp
 
 bool CreateRegistryValue()
 {
-    // Получение выбранного типа данных из выпадающего списка
+
     int selectedIndex = SendMessage(hComboBox, CB_GETCURSEL, 0, 0);
     DWORD valueType = static_cast<DWORD>(SendMessage(hComboBox, CB_GETITEMDATA, selectedIndex, 0));
 
-    // Получение введенного значения данных из поля ввода
     char valueData[256];
     GetWindowTextA(hValueDataEdit, valueData, sizeof(valueData));
 
-    // Получение введенного имени значения из поля ввода
     char valueName[256];
     GetWindowTextA(hValueNameEdit, valueName, sizeof(valueName));
 
-    // Создание нового значения в реестре
     HKEY hKey;
-    LPCSTR subKey = "Software\\MyApp";
-    LONG openResult = RegCreateKeyExA(HKEY_CURRENT_USER, subKey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKey, NULL);
+    int length = GetWindowTextLength(hEdit);
+    //LPWSTR subKey = new WCHAR[length + 1];
+    //GetWindowText(hEdit, subKey, length + 1);
+    LPWSTR buffer = new WCHAR[length + 1];
+    GetWindowText(hEdit, buffer, length + 1);
+    //LPCSTR subKey = "Software\\MyApp";
+    LONG openResult = RegCreateKeyEx(HKEY_CURRENT_USER, buffer, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKey, NULL);
 
     if (openResult != ERROR_SUCCESS) {
         MessageBox(NULL, L"Failed to create registry key", L"Error", MB_ICONERROR);
@@ -403,22 +438,17 @@ bool CreateRegistryValue()
     LONG setValueResult;
     if (valueType == 4) {
 
-        DWORD result;
-        char* endPtr; // Указатель, указывающий на первый символ, не преобразованный в число
+        DWORD result;   
+        char* endPtr; 
 
         result = strtoul(valueData, &endPtr, 10);
 
         if (*endPtr == '\0') {
-            // Преобразование прошло успешно, строка была полностью преобразована в число
-            // result теперь содержит число в формате DWORD
         }
         else {
-            // Преобразование не удалось, строка содержит нечисловые символы
-            // Обработка ошибки здесь
         }
-        /*setValueResult = RegSetValueExA(hKey, valueName, 0, valueType, reinterpret_cast<const BYTE*>(result), strlen(valueData) + 1);*/
-        BYTE data[sizeof(DWORD)]; // Создаем буфер для хранения данных DWORD
-        memcpy(data, &result, sizeof(DWORD)); // Копируем DWORD в буфер
+        BYTE data[sizeof(DWORD)]; 
+        memcpy(data, &result, sizeof(DWORD));
         setValueResult = RegSetValueExA(hKey, valueName, 0, REG_DWORD, data, sizeof(DWORD));
     }
     else if (valueType == 1) {
@@ -439,8 +469,8 @@ bool CreateRegistryValue()
 }
 
 void DeleteSubkeys(HKEY hKey) {
-    TCHAR achKey[255]; // Буфер для имени подключа
-    DWORD cbName;      // Длина имени подключа
+    TCHAR achKey[255];
+    DWORD cbName;
     LONG lResult;
 
     while (true) {
@@ -457,6 +487,49 @@ void DeleteSubkeys(HKEY hKey) {
                 RegCloseKey(hSubKey);
             }
         }
+    }
+}
+
+void getRegistryValue(LPCTSTR subKey, LPCTSTR name)
+{
+    HKEY hKey;
+    DWORD dwType = 0;
+    BYTE data[1024];
+    DWORD dwDataSize = sizeof(data);
+    LONG lReturn;
+
+    lReturn = RegOpenKeyEx(HKEY_CURRENT_USER, subKey, 0, KEY_READ, &hKey);
+
+    if (lReturn == ERROR_SUCCESS)
+    {
+        lReturn = RegQueryValueEx(hKey, name, NULL, &dwType, data, &dwDataSize);
+
+        if (lReturn == ERROR_SUCCESS)
+        {
+            switch (dwType)
+            {
+            case REG_SZ:
+                MessageBoxA(NULL, reinterpret_cast<const char*>(data), "Строковое значение реестра", MB_OK | MB_ICONINFORMATION);
+                break;
+            case REG_DWORD:
+                char buffer[32];
+                sprintf_s(buffer, sizeof(buffer), "%lu", *reinterpret_cast<DWORD*>(data));
+                MessageBoxA(NULL, buffer, "DWORD значение реестра", MB_OK | MB_ICONINFORMATION);
+                break;
+            default:
+                MessageBoxA(NULL, "Неизвестный тип данных.", "Ошибка", MB_OK | MB_ICONERROR);
+            }
+        }
+        else
+        {
+            MessageBoxA(NULL, "Не могу получить значение параметра из реестра.", "Ошибка", MB_OK | MB_ICONERROR);
+        }
+
+        RegCloseKey(hKey);
+    }
+    else
+    {
+        MessageBoxA(NULL, "Ключ реестра не открывается.", "Ошибка", MB_OK | MB_ICONERROR);
     }
 }
 
